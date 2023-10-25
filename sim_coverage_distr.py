cd45cc729928e0432ae376d6af2bc787f52a52ac
NikolasVellnow <42894685+NikolasVellnow@users.noreply.github.com>
2023-10-25 17:22:24 +0200

    Changed classes so that they now follow an inheritance hierarchy

--- sim_coverage_distr.py
+++ sim_coverage_distr.py
@@ -1,7 +1,6 @@
 
 from dataclasses import dataclass
 from timeit import default_timer as timer
-from typing import Any
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -64,49 +63,16 @@ class GenomeM2(Genome):
         super().__init__(size)
         self.rel_size_p = rel_size_p
         self.site_grc_mappings = np.full(self.size, False)
-        self.G_G = int(self.rel_size_p * self.size)
+        self.size_grc_region = int(self.rel_size_p * self.size)
         # GRC sequences are at beginning of the chromosomes...
-        self.grc_sites = np.arange(0, self.G_G)
+        self.grc_sites = np.arange(0, self.size_grc_region)
         self.site_grc_mappings[self.grc_sites] = True
 
+    def get_rel_size_p(self) -> float:
+        return self.rel_size_p
 
-class Genome_m2:
-    """ Class to represent reference genome according to model 2"""
-
-    def get_grc_mapping(self, position):
-        """ Returns boolean whether grc maps to site or not """
-        return self.site_grc_mappings[position]
-
-    def map_reads(self, num_reads, read_len=150):
-        """ Simulates a read mapping process """
-        tmp_read = Read(length=read_len)
-        for _ in range(0, num_reads):
-            rnd_float = rnd.random()  # generate random float between 0.0 and 1.0
-            # Read has probability of p/(1+p) to come from GRC
-            if rnd_float < (self.rel_size_p/(1+self.rel_size_p)):
-                tmp_read = Read(length=read_len, maps_to_grc=True)
-                # TODO self.grc.sites ...
-                rnd_pos = rnd.randint(0, self.G_G - (tmp_read.length-1))
-            else:
-                tmp_read = Read(length=read_len)
-                rnd_pos = rnd.randint(0, self.size - (tmp_read.length-1))
-            read_sites = np.arange(rnd_pos, rnd_pos+tmp_read.length)
-            self.site_depths[read_sites] += 1
-
-    # TODO Write method to save the data
-
-    def plot_coverage_hist(self, bins, read_length=150):
-        """ Plots a histogram of simulated read coverage """
-        # We exclude the beginning and end of genome to exclude edge effects
-        plt.hist(
-            self.site_depths[read_length: (self.size-read_length):1], bins=bins, density=False)
-        plt.show()
-
-    def cov_coeff_var(self, read_length=150):
-        "Calculates the coefficient of variation od the coverage values"
-        coeff_var = variation(
-            self.site_depths[read_length: (self.size-read_length):1], axis=0, ddof=1)
-        return coeff_var
+    def get_size_grc_region(self) -> int:
+        return self.size_grc_region
 
 
 class GenomeMapping:
@@ -136,11 +102,11 @@ class GenomeMapping:
 
     def map_reads(self, num_reads, read_len=150):
         """ Simulates a read mapping process generally """
-        tmp_read = Read(length=read_len)
+        # tmp_read = Read(length=read_len) # Do i need the Read class?
         for _ in range(0, num_reads):
             rnd_pos = rnd.randint(
-                0, self.genome.get_size() - (tmp_read.length-1))
-            read_sites = np.arange(rnd_pos, rnd_pos+tmp_read.length)
+                0, self.genome.get_size() - (read_len-1))
+            read_sites = np.arange(rnd_pos, rnd_pos+read_len)
             self.site_depths[read_sites] += 1
 
     def plot_coverage_hist(self, bins, read_length=150):
@@ -162,27 +128,15 @@ class GenomeMapping:
 
 class GenomeMappingM1(GenomeMapping):
     """ Class to represent read mapping process according to model 1 (null model)"""
+    pass
 
 
-class GenomeMappingM2:
+class GenomeMappingM2(GenomeMapping):
     """ Class to represent reference genome according to model 2"""
 
-    def __str__(self) -> str:
-        site_list = list(zip(self.site_grc_mappings, self.site_depths))
-        site_list = map(str, site_list)
-        return "\n".join(site_list)
-
-    def get_depth(self, position):
-        """ Returns site object at specified position"""
-        return self.site_depths[position]
-
-    def set_depth(self, position, new_site):
-        """ Updates site object at specified position"""
-        self.site_depths[position] = new_site
-
-    def get_grc_mapping(self, position):
-        """ Returns boolean whether grc maps to site or not """
-        return self.site_grc_mappings[position]
+    def __init__(self, genome: GenomeM2):
+        self.genome = genome
+        self.site_depths = np.zeros(self.genome.get_size(), dtype=np.int16)
 
     def map_reads(self, num_reads, read_len=150):
         """ Simulates a read mapping process """
@@ -190,30 +144,32 @@ class GenomeMappingM2:
         for _ in range(0, num_reads):
             rnd_float = rnd.random()  # generate random float between 0.0 and 1.0
             # Read has probability of p/(1+p) to come from GRC
-            if rnd_float < (self.p/(1+self.p)):
-                tmp_read = Read(length=read_len, maps_to_grc=True)
-                # TODO self.grc.sites ...
-                rnd_pos = rnd.randint(0, self.G_G - (tmp_read.length-1))
+            p_from_grc = self.genome.get_rel_size_p()/(1+self.genome.get_rel_size_p())
+            if rnd_float < (p_from_grc):
+                # tmp_read = Read(length=read_len, maps_to_grc=True) # Do I need the Read class?
+                rnd_pos = rnd.randint(
+                    0, self.genome.get_size_grc_region() - (read_len-1))
             else:
-                tmp_read = Read(length=read_len)
-                rnd_pos = rnd.randint(0, self.size - (tmp_read.length-1))
+                # tmp_read = Read(length=read_len)
+                rnd_pos = rnd.randint(
+                    0, self.genome.get_size() - (read_len-1))
             read_sites = np.arange(rnd_pos, rnd_pos+tmp_read.length)
             self.site_depths[read_sites] += 1
 
     # TODO Write method to save the data
 
-    def plot_coverage_hist(self, bins, read_length=150):
-        """ Plots a histogram of simulated read coverage """
-        # We exclude the beginning and end of genome to exclude edge effects
-        plt.hist(
-            self.site_depths[read_length: (self.size-read_length):1], bins=bins, density=False)
-        plt.show()
+    # def plot_coverage_hist(self, bins, read_length=150):
+    #     """ Plots a histogram of simulated read coverage """
+    #     # We exclude the beginning and end of genome to exclude edge effects
+    #     plt.hist(
+    #         self.site_depths[read_length: (self.genome.get_size()-read_length):1], bins=bins, density=False)
+    #     plt.show()
 
-    def cov_coeff_var(self, read_length=150):
-        "Calculates the coefficient of variation od the coverage values"
-        coeff_var = variation(
-            self.site_depths[read_length: (self.size-read_length):1], axis=0, ddof=1)
-        return coeff_var
+    # def cov_coeff_var(self, read_length=150):
+    #     "Calculates the coefficient of variation of the coverage values"
+    #     coeff_var = variation(
+    #         self.site_depths[read_length: (self.genome.get_size()-read_length):1], axis=0, ddof=1)
+    #     return coeff_var
 
 
 def main():
@@ -231,20 +187,41 @@ def main():
     t0 = timer()
     g1 = GenomeM1(size=G_R)
     t1 = timer()
-    print(f'Initialize genome in {t1-t0} s')
+    print(f'Initialize GenomeM1 in {t1-t0} s')
 
     gm1 = GenomeMappingM1(genome=g1)
+
     t2 = timer()
     gm1.map_reads(num_reads=n_reads, read_len=L)
     t3 = timer()
-    print(f'Map reads in {t3-t2} s')
+    print(f'GenomeMappingM1 in: {t3-t2} s')
 
     print("GenomeM1:\n", g1)
-    print("GenomeMapping:\n", gm1)
+    print("GenomeMapping1:\n", gm1)
 
-    print(f'Coefficient of variation: {gm1.cov_coeff_var(read_length=L)}')
+    print(
+        f'Coefficient of variation of Coverage of GenomeM1: {gm1.cov_coeff_var(read_length=L)}')
+
+    t4 = timer()
+    g2 = GenomeM2(size=G_R, rel_size_p=rel_size_p)
+    t5 = timer()
+    print(f'Initialize GenomeM2 in {t5-t4} s')
+
+    gm2 = GenomeMappingM2(genome=g2)
+
+    t6 = timer()
+    gm2.map_reads(num_reads=n_reads, read_len=L)
+    t7 = timer()
+    print(f'GenomeMappingM2 in: {t7-t6} s')
+
+    print("GenomeM2:\n", g2)
+    print("GenomeMapping2:\n", gm2)
+
+    print(
+        f'Coefficient of variation of Coverage of GenomeM2: {gm2.cov_coeff_var(read_length=L)}')
 
     gm1.plot_coverage_hist(bins=n_bins, read_length=L)
+    # gm2.plot_coverage_hist(bins=n_bins, read_length=L)
 
     # TODO create useful test cases, e.g. several instances that are independtenly manipulated etc.
 
